36402f0d62f277d367908ccaac3cfc14aaf1614a
SergiiPototskyi <sergiipototskyi@gmail.com>
2016-08-03 16:19:18 +0300

    Добавили управление клавишами

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace snake
 {
@@ -28,7 +29,19 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+
 
 
             Console.ReadLine();
