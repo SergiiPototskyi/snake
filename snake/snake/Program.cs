a838bf8090b3bc2ca0af240f6cf2db47125c98ff
SergiiPototskyi <sergiipototskyi@gmail.com>
2016-08-03 13:05:58 +0300

    Нарисовали змейку

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -26,7 +26,8 @@ namespace snake
             rightline.Drow();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
 
             Console.ReadLine();
