e2430a8da2374a3534aaa66a1b26d45a609c7526
SergiiPototskyi <sergiipototskyi@gmail.com>
2016-08-03 11:49:39 +0300

    Создали класс HorisontalLine, чтобы нарисовать линию

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,43 +10,16 @@ namespace snake
     {
         static void Main(string[] args)
         {
+
             Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-
-            Point p3 = new Point(8, 16, '@');
-
-            Point p4 = new Point(15, 7, '%');
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-
-
-            p1.Draw();
             p2.Draw();
-            p3.Draw();
-            p4.Draw();
 
+            HorisontalLine line = new HorisontalLine(5, 10, 8, '+');
 
+            line.Drow();
 
             Console.ReadLine();
         }
