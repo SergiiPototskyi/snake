e3da280b1ea86813566ec9f14b787984fd0f1f8c
SergiiPototskyi <sergiipototskyi@gmail.com>
2016-08-04 10:54:32 +0300

    Обработали столкновения со стенами и самой змейкой

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,6 +11,41 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while(true)
+            {
+                if(walls.IsHit(snake) || snake.IsHitTail());
+                {
+                    break;
+                }
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+            }
+
+
+
+
             VerticalLine v1 = new VerticalLine(5, 0, 10, '%');
             Draw(v1);
 
