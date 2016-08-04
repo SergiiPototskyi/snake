6d5a9d7bb590091611676e2cea9351df4d7663f6
SergiiPototskyi <sergiipototskyi@gmail.com>
2016-08-04 11:24:21 +0300

    Змейка готова

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -25,13 +25,13 @@ namespace snake
             Point food = foodCreator.CreateFood();
             food.Draw();
 
-            while(true)
+            while (true)
             {
-                if(walls.IsHit(snake) || snake.IsHitTail());
+                if (walls.IsHit(snake) || snake.IsHitTail())
                 {
                     break;
                 }
-                if(snake.Eat(food))
+                if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
@@ -40,38 +40,15 @@ namespace snake
                 {
                     snake.Move();
                 }
+
                 Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
             }
-
-
-
-
-            VerticalLine v1 = new VerticalLine(5, 0, 10, '%');
-            Draw(v1);
-
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-
-            HorisontalLine h1 = new HorisontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
-
-            Console.ReadLine();
-        }
-
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
         }
     }
 }
